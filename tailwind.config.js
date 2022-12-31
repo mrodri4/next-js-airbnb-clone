c57e1c17e4d18298710d184e544647a5efd6c053
Manuel Rodriguez <mrodriguez@avantio.com>
2022-12-31 13:58:27 +0100

    chore(tailwindcss): Add Airbnb color as a tailiwind color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'airbnb-color': '#FF385C'
+      }
+    },
   },
   plugins: [],
 }
